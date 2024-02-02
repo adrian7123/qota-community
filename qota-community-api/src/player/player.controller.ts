b9ff7b7ff1da8a35469fd31f41d757d1ce73a0f0
adrian7123 <bueno1dad@gmail.com>
2024-02-02 02:14:08 +0000

    add score and new channels

--- qota-community-api/src/player/player.controller.ts
+++ qota-community-api/src/player/player.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Param, Post } from '@nestjs/common';
+import axios from 'axios';
 import { PrismaService } from 'src/prisma.service';
 
 @Controller('player')
@@ -18,5 +19,48 @@ export class PlayerController {
     score = player.score;
 
     return score;
+  }
+
+  @Get("/all")
+  async GetAllPlayer() {
+    let players = await this.prisma.player.findMany({
+      orderBy: {
+        score: 'desc'
+      }
+    });
+    let playersResult = [];
+
+    const steamIDs = players.map(
+      (player) => player.steamID,
+    );
+
+    const steam_web_key = process.env.STEAM_WEB_KEY;
+
+    try {
+      const res = await axios.get(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/`, {
+        params: {
+          key: steam_web_key,
+          steamids: steamIDs.join(","),
+        }
+      });
+
+      const profiles: any[] = res.data.response.players;
+
+      console.log(profiles);
+
+      players.forEach((player) => {
+        const steamProfile = profiles.find((p) => p.steamid === player.steamID);
+
+        playersResult.push({
+          ...player,
+          ...steamProfile
+        });
+      });
+
+    } catch (e) {
+      console.error(e);
+    }
+
+    return playersResult;
   }
 }
