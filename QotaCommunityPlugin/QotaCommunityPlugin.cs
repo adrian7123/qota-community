4cbc6fe9c13bc012b37b53412edf9db90c157ae8
Adrian Bueno <32718131+adrian7123@users.noreply.github.com>
2024-02-02 01:12:57 -0300

    up

--- QotaCommunityPlugin/QotaCommunityPlugin.cs
+++ QotaCommunityPlugin/QotaCommunityPlugin.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes;
@@ -19,42 +20,117 @@ public class QotaCommunityPlugin : BasePlugin
   public override string ModuleAuthor => "https://github.com/adrian7123 (Adrian Bueno)";
   public override string ModuleDescription => "Qota Community Plugin";
 
-  private readonly SocketIO socketClient = new SocketIO("https://f17c-170-79-148-67.ngrok-free.app");
+  private readonly SocketIO socketClient = new SocketIO("http://15.229.76.232:3033");
+
+  private readonly HttpClient client = new HttpClient();
+
+  private readonly string qotaPrefix = $"[{ChatColors.Green}Qota Community{ChatColors.Default}] ";
 
   public override void Load(bool hotReload)
   {
     Logger.LogInformation("Qota Community Plugin loading!");
 
-    Task.Run(socketClient.ConnectAsync);
-
-    // Subscriptions can be added via the instance method
-    RegisterEventHandler<EventPlayerDeath>((@event, info) =>
+    Task.Run(async () =>
     {
+      await socketClient.ConnectAsync();
+      Logger.LogInformation("Qota Community Socket Connected");
+    });
 
-      Logger.LogInformation("Event player death");
+  }
 
-      Task.Run(async () =>
+  [GameEventHandler(HookMode.Pre)]
+  public HookResult OnPlayerConnect(EventPlayerConnect @event, GameEventInfo info)
+  {
+
+    Logger.LogInformation($"@Qota Community Player {@event.Userid.SteamID} Connected");
+
+    socketClient.EmitAsync("player connected", new Dictionary<string, dynamic> {
+      {"name", @event.Userid.PlayerName},
+      {"steamID", $"{@event.Userid.SteamID}"}
+    });
+
+    return HookResult.Continue;
+  }
+
+  [GameEventHandler(HookMode.Pre)]
+  public HookResult OnPlayerDeath(EventPlayerDeath @event, GameEventInfo info)
+  {
+    Logger.LogInformation($"Event player death {@event.Attacker.PlayerName}");
+
+    socketClient.EmitAsync("player kill", new Dictionary<string, dynamic> {
+      {"killedName",@event.Userid.PlayerName},
+      {"killedSteamID",$"{@event.Userid.SteamID}"},
+      {"killerName",@event.Attacker.PlayerName},
+      {"killerSteamID",$"{@event.Attacker.SteamID}"},
+      {"headshot",@event.Headshot},
+      {"weapon",@event.Weapon},
+    });
+
+    double score = 30;
+
+    if (@event.Weapon.Contains("knife"))
+    {
+      score = 100;
+    }
+    else if (@event.Weapon.Contains("tazer"))
+    {
+      score = 80;
+    }
+    else if (@event.Weapon.Contains("deagle"))
+    {
+      score = 60;
+    }
+
+    if (@event.Headshot)
+    {
+      score += 20;
+    }
+
+    var s = Convert.ToInt32(score / 3);
+
+    var shotguns = new List<string> {
+      "mag7",
+      "sawedoff",
+      "nova",
+      "xm1014"
+    };
+
+    if (shotguns.Contains(@event.Weapon))
+    {
+      score = 0.1;
+    }
+
+    if (@event.Weapon.Contains("knife"))
+    {
+      s = 200;
+    }
+
+    @event.Attacker.PrintToChat($"{qotaPrefix}Matou {ChatColors.Green}+{score}{ChatColors.Green}");
+    @event.Userid.PrintToChat($"{qotaPrefix}Morreu {ChatColors.Red}-{s}{ChatColors.Red}");
+
+    return HookResult.Continue;
+  }
+
+  [ConsoleCommand("points", "@Qota Community Get Points")]
+  public void GetPoints(CCSPlayerController? player, CommandInfo command)
+  {
+
+    Logger.LogInformation("GetPoints");
+
+    if (player == null) return;
+
+    Task.Run(async () =>
+    {
+      var res = await client.GetAsync($"http://api.rank.buenoo.online/player/score/{player?.SteamID}");
+
+      if (res.IsSuccessStatusCode)
       {
-        object[] data = {
-          @event.Attacker.ClanName,
-          @event.Attacker.SteamID,
-          @event.Userid.ClanName,
-          @event.Userid.SteamID,
-        };
+        var score = res.Content.ToString();
+        Logger.LogInformation(score);
 
-        await socketClient.EmitAsync("kill", data);
-      });
-
-      // You can use `info.DontBroadcast` to set the dont broadcast flag on the event (in pre handlers)
-      // This will prevent the event from being broadcast to other clients.
-      // In this example we prevent kill-feed messages from being broadcast if it was not a headshot.
-      if (!@event.Headshot)
-      {
-
+        player?.PrintToChat($"{qotaPrefix}seu score é {score}");
       }
-
-      return HookResult.Continue;
-    }, HookMode.Pre);
+    });
   }
 
   [ConsoleCommand("qc_all_to_spec", "@Qota Community Set All Players to Spec")]
