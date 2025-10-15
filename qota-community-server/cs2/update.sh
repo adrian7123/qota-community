46ec288af85615ccd63a3d9ccdcc1a7001350b21
Adrian Bueno <adrian.s.bueno1@gmail.com>
2025-10-15 09:44:40 -0300

    remove Dockerfile, scripts de inicialização e atualização do servidor CS2

--- qota-community-server/cs2/update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-/home/steam/steamcmd/steamcmd.sh +@ShutdownOnFailedCommand 1 +@NoPromptForPassword 1 \
-        +@sSteamCmdForcePlatformType linux \
-        +login anonymous +force_install_dir "/home/steam/cs2/" \
-        +app_update 730 +quit +exit
